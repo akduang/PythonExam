b9802fee0977653a4494def36b267d8794ed370c
akduang <2019364641@qq.com>
2023-07-17 14:48:50 +0800

    update

--- Exam.py
+++ Exam.py
@@ -1,7 +1,17 @@
-#1
+# #1
 
-def hello():
-    return "Hello, World!"
+# def hello():
+#     return "Hello, World!"
   
-# Too slow 
-# change
+# # Too slow 
+# # change
+
+a = [3,1,1]
+range(10)
+# [1,10)
+b = list((3,1,2))
+c = list(range(10))
+print(type(a),type(b),type(c))
+
+#正索引(0-5)，负索引(-5--1)
+#切片  : s
